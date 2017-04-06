9a388e4be638d663b51955f0b3c65d5d155520c0
Jordan Mahar <maharjordan@gmail.com>
2017-04-06 16:00:40 -0300

    Updated Water.cpp file
    After  completing the base problem set, I decided to expand upon it.
    Added a caption to the console, allowed for partial value by switching to doubles, and now it reports bottles of water used per shower, year, and over the average Canadian life span.

--- water.cpp
+++ water.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <limits>
+#include <cstring>
 using namespace std;
 
 #define BOTTLE 12
+#define title "Shower Water Usage Calculator"
+
+void print_title(string input_title);
 
 int main()
-{
-	cout <<"Minutes: ";
-	int minutes{ -1 };	
+{	
+	
+	print_title(title);
+	cout <<"How long is your average shower in minutes?: ";
+	double minutes{ -1 };	
 
 	while( !(cin >> minutes) || minutes < 0 )
 	{
@@ -19,8 +25,58 @@ int main()
             <<"Option: ";
         }
 
-	int bottles = minutes * BOTTLE;
-
-	cout << "Bottles: " << bottles;
+	double bottles = minutes * BOTTLE;
+	double bottles_year = 365 * bottles;
+    //Using Canadian life expectency of 81.24 years (Source World Bank)
+    long bottles_life = 81.24 * (int)bottles_year;
+    
+    cout << "Your expected usage of water in bottles from showering is:" << endl;
+    cout << "----------------------------------------------------------" << endl;
+	cout << "Per Shower: " << bottles <<endl;
+	cout << "Per Year:   " << bottles_year << endl;
+	cout << "Life-time:  " << bottles_life << endl;
 	return 0;
+}
+
+//Prints an Asterisk border around a string of text
+void print_title(string input_title){
+    //Get the length of the string
+    int len = input_title.size();
+    
+    //Print the top line
+    for(int i = 0; i < len + 4; i++){
+        cout << '*';
+    }
+    cout << endl;
+    
+    //Print the side border
+    cout << "* ";
+    
+    for (int i = 0; i < len; i++){
+        cout << " ";   
+    }
+    
+    cout << " *" << endl;
+    
+    //Print the title
+    cout << "* " << input_title << " *" << endl;
+    
+    //Print the side border
+    cout << "* ";
+    
+    for (int i = 0; i < len; i++){
+        cout << " ";   
+    }
+    
+    cout << " *" << endl;
+    
+    //Print the bottom border
+    for(int i = 0; i < len + 4; i++){
+        cout << '*';
+    }
+    cout << endl;
+    
+    
+    
+    
 }
