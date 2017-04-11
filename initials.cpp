368f84b150d5ab1a2c90f244b0dc76576c4213a3
Jordan Mahar <maharjordan@gmail.com>
2017-04-11 12:28:16 -0300

    Update initials.cpp
    Moved the initials section into its own function.
    
    Made sure initials were printed as capitals

--- initials.cpp
+++ initials.cpp
@@ -3,11 +3,20 @@
 #include <string>
 using namespace std;
 
+string initials(string name);
+
 int main(){
 	//Get the name from the user
 	string name;
-	string initials{""};
 	getline (cin, name);
+	
+	cout << initials(name) << endl;
+	
+	return 0;
+}
+
+string initials(string name){
+	string inits{""};
 	int len = name.length();
 	
 	//Get the initials
@@ -18,18 +27,16 @@ int main(){
 		//If on the first character, test to see if it is a letter
 		if( i == 0 ){
 			if ( isalpha(let_one) ){
-				initials += let_one;
+				inits += toupper(let_one);
 			}
 		}
 		//If the previous letter was a space, check to see if the next is a letter
 		else{
 			if( let_one == ' ' && isalpha(let_two) ){
-				initials += let_two;
+				inits += toupper(let_two);
 			}
 		}
 	}
 	
-	cout << initials << endl;
-	
-	return 0;
+	return inits;
 }
