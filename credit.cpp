e3cd112a6604b255c69cadd72b4b50dd281d96ca
Jordan Mahar <maharjordan@gmail.com>
2017-04-11 11:40:10 -0300

    Finished the base program
    Program meets specifications of problem set in CS50

--- credit.cpp
+++ credit.cpp
@@ -28,12 +28,26 @@ int main(){
 	//Test the number of digits
 	int digit_num = digits(cNumber);
 	
-	if( digit_num < 13 || digit_num > 16 || digit_num == 14){
-		cout << "INVALID" << endl;
+	if( luhn(cNumber) == false ){
+		cout <<"INVALID" << endl;
 		return 0;
 	}
+
+	if( (digit_num == 13 || digit_num == 16 ) && digit_spec(cNumber,1) == 4 ){
+		cout << "VISA" << endl;
+	}
 	
+	else if( digit_num == 16 && digit_spec(cNumber,1) == 5 && digit_spec(cNumber,2) >= 1 && digit_spec(cNumber,2) <= 5){
+		cout << "MASTERCARD" << endl;
+	}
 	
+	else if( digit_num == 15 && digit_spec(cNumber,1) == 3 && (digit_spec(cNumber,2) == 4 || digit_spec(cNumber,2) == 7) ){
+		cout << "AMEX" << endl;
+	}
+	
+	else{
+		cout << "INVALID END" << endl;
+	}
 	
 	return 0;
 }
@@ -71,7 +85,7 @@ int digit_spec(long long x, int pos){
 	
 	pos = digit_num - pos + 1;
 	
-	int base = pow(10, pos - 1);
+	long long base = pow(10, pos - 1);
 	
 	x /= base;
 	x %= 10;
@@ -83,11 +97,11 @@ bool luhn(long long x){
     int digit_num = digits(x);
     int sum { 0 };
     
-    for (int i = 1; i < digit_num; i += 2){
+    for (int i = digit_num; i > 0; i -= 2){
         sum += digit_spec(x, i);   
     }
     
-    for (int i = 2; i < digit_num; i+= 2){
+    for (int i = digit_num - 1; i > 0; i-= 2){
         int place = digit_spec(x,i) * 2;
         if( place < 10){
             sum += place;
@@ -97,7 +111,6 @@ bool luhn(long long x){
             sum += (place % 10);
         }
     }
-    
     if( sum % 10 == 0) return true;
     else return false;
 }
